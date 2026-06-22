94702cdf7e1e0fdbc861d364bd759c995dd70bd1
Md Hussain Ahmed <amdhussain538@gmail.com>
2026-06-22 21:28:37 +0600

    added a new code

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,21 +1,10 @@
- 
-
-
 
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL 
 })
 
 
 
-// import { createAuthClient } from "better-auth/react";
-
-// export const authClient = createAuthClient({
-//   // baseURL: "https://pixgen-chi.vercel.app" 
-//    baseURL: process.env.BETTER_AUTH_URL 
-// });
-
-// export const { signIn, signOut, useSession } = authClient;
-
+ 
